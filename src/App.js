a0716fd947210334b63ae5f7803150e501bc5268
shagunarora <shagunarora2601@gmail.com>
2019-07-23 16:49:55 +0530

    Component App

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import Table from "./Table";
+import Form from "./Form";
+
+class App extends Component {
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+  removeCharacter = index => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    });
+  };
+  state = {
+    characters: []
+  };
+  render() {
+    const { characters } = this.state;
+
+    return (
+      <div className="container">
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
+export default App;
